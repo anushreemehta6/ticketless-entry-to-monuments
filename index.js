b08dd2f1dff39a8217d49d0d464774498825d4a0
Anushree mehta <104919688+anushreemehta6@users.noreply.github.com>
2024-09-09 20:00:17 +0530

    js added

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+
+
 let btn = document.getElementById('btn');
 // let overlay = document.getElementsByClassName('overlay');
 let overlay = document.getElementById('overlay');
 function popup(){
     overlay.style.display = "block";
 }
+
+ 
